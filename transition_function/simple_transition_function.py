2a04278078f2c2e9a6017d6ce98db7c79b60b0fd
irfansha <irfansha536@gmail.com>
2021-04-08 09:30:59 +0200

    Started map generator with todos.

--- transition_function/simple_transition_function.py
+++ transition_function/simple_transition_function.py
@@ -5,13 +5,26 @@ from utils.variables_dispatcher import VarDispatcher as vd
 
 class SimpleTransitionFunction:
 
+  def map_generator(self):
+    # TODO: generate log action variables and add to map
+    # TODO: generate log varibles for parameters and add to map
+    # TODO: generate forall variables
+    # TODO: generate static variables and add to map
+    # TODO: generate non-static variables of two time steps and add to map
+    print(self.variables_map)
+
+
   def __init__(self, parsed_instance):
-    probleminfo = pinfo(parsed_instance)
-    print(probleminfo)
+    self.probleminfo = pinfo(parsed_instance)
+    self.variables_map = dict()
+
+    #print(probleminfo)
     # Using variable dispatcher for new integer variables:
     transition_variables = vd()
 
     # TODO: map generator function from variables to integer variables
+    self.map_generator()
+
 
     # TODO: using the map and new gates generator, add new transition generator
 
