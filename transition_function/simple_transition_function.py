5d794f8c0a0975ef82045caf307172442b0171ab
irfansha <irfansha536@gmail.com>
2021-04-08 09:12:09 +0200

    Started using variable dispatcher.

--- transition_function/simple_transition_function.py
+++ transition_function/simple_transition_function.py
@@ -1,12 +1,15 @@
 # Irfansha Shaik, 07.04.2021, Aarhus
 
 from constraints.problem_structure import ProblemInfo as pinfo
+from utils.variables_dispatcher import VarDispatcher as vd
 
 class SimpleTransitionFunction:
 
   def __init__(self, parsed_instance):
     probleminfo = pinfo(parsed_instance)
     print(probleminfo)
+    # Using variable dispatcher for new integer variables:
+    transition_variables = vd()
 
   def __str__(self):
     return 'TODO'
