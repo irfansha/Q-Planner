c5a32b612028a58c91ce9806208c2851233c689e
irfansha <irfansha536@gmail.com>
2021-05-03 09:21:35 +0200

    Providing valid invariants seperately.

--- run/run_cpddl.py
+++ run/run_cpddl.py
@@ -41,12 +41,35 @@ class RunCpddl:
         self.parsed_mutex_groups.append(cur_parsed_mutex_group)
 
 
+  # Invariants with invalid parameter types are dropped:
+  def filter_valid_invariants(self, tfunc):
+    for mutex_group in self.parsed_mutex_groups:
+      valid_single_mutex_group = []
+      for single_invariant in mutex_group:
+        valid_invariant_flag = 1
+        # Checking if all the arguments in the invariant have valid type:
+        for i in range(1, len(single_invariant)):
+          # First element in the tuple gives the invariant type:
+          cur_invariant_type = single_invariant[i][0]
+          # Second element in the tuple gives the paramter type:
+          cur_invariant_parameter_type = single_invariant[i][1]
+          # We only need to filter general types, constants are fine:
+          if (cur_invariant_type != 'const' and cur_invariant_parameter_type not in tfunc.parsed_instance.valid_type_names):
+              # Making invariant invalid:
+              valid_invariant_flag = 0
+        if (valid_invariant_flag != 0):
+          valid_single_mutex_group.append(single_invariant)
+      # Dropping some invariants results in redundant invariants:
+      if (valid_single_mutex_group not in self.valid_mutex_groups):
+        self.valid_mutex_groups.append(valid_single_mutex_group)
+
   def __init__(self, tfunc):
     self.domain_path = os.path.join(tfunc.parsed_instance.args.path, tfunc.parsed_instance.args.domain)
     self.problem_path = os.path.join(tfunc.parsed_instance.args.path, tfunc.parsed_instance.args.problem)
     self.cpddl_tool_path = os.path.join(tfunc.parsed_instance.args.planner_path, 'tools', 'cpddl' , 'pddl-lifted-mgroups')
     self.invariants_out = tfunc.parsed_instance.args.invariants_out
     self.parsed_mutex_groups = []
+    self.valid_mutex_groups = []
     self.run()
     # TODO: Parse and update the invariants generated
     self.parse()
@@ -56,5 +79,10 @@ class RunCpddl:
       for mutex_group in self.parsed_mutex_groups:
         print(mutex_group)
     '''
-    for mutex_group in self.parsed_mutex_groups:
+    #for mutex_group in self.parsed_mutex_groups:
+    #  print(mutex_group)
+
+    self.filter_valid_invariants(tfunc)
+
+    for mutex_group in self.valid_mutex_groups:
       print(mutex_group)
